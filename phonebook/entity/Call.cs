cacef11f32d4b704fe84dcd72f9c2951183622a9
Bartol Deak <b@bdeak.net>
2021-11-25 00:19:59 +0100

    add Call and Contact classes

---
+++ phonebook/entity/Call.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phonebook.entity
+{
+    class Call
+    {
+        public enum Status
+        {
+            InProgress,
+            Missed,
+            Ended,
+        };
+
+        private DateTime _date;
+        private Status _status = Status.InProgress;
+
+        public Call()
+        {
+            _date = DateTime.Now;
+        }
+
+        public Call(DateTime date, Status status)
+        {
+            _date = date;
+            _status = status;
+        }
+
+    }
+}
---
+++ phonebook/entity/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phonebook.entity
+{
+    class Contact
+    {
+        public enum Type
+        {
+            Normal,
+            Favorite,
+            Blocked,
+        };
+
+        private string _name;
+        private string _number;
+        private Type _type = Type.Normal;
+
+        public Contact(string name, string number)
+        {
+            _name = name;
+            _number = number;
+        }
+
+        public Contact(string name, string number, Type type)
+        {
+            _name = name;
+            _number = number;
+            _type = type;
+        }
+
+    }
+}
