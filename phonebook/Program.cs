449071f132fccd0b401653b0a71a190eb16ad135
Bartol Deak <b@bdeak.net>
2021-11-25 00:22:52 +0100

    add default data

--- phonebook/Program.cs
+++ phonebook/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using phonebook.entity;
 
 namespace phonebook
 {
@@ -7,9 +8,14 @@ namespace phonebook
     {
         static void Main(string[] args)
         {
-            var data = new Dictionary<string, string>
+            var data = new Dictionary<Contact, Call[]>
             {
-                {"a", "b"},
+                { new Contact("Ante Antic", "0981234567", Contact.Type.Favorite),
+                  new Call[] { new Call(new DateTime(2021, 2, 2), Call.Status.Ended), new Call() } },
+                { new Contact("Ana Anic", "0951234567"),
+                  new Call[] { new Call(new DateTime(2021, 11, 11), Call.Status.Ended) } },
+                { new Contact("Mate Matic", "0991234567", Contact.Type.Blocked),
+                  new Call[] { } },
             };
         }
     }
