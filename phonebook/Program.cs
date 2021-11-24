3b74bdda9c9e7bcc28aed8626d49d69d9826de1c
Bartol Deak <b@bdeak.net>
2021-11-24 23:12:16 +0100

    init

---
+++ phonebook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, string>
+            {
+                {"a", "b"},
+            };
+        }
+    }
+}
