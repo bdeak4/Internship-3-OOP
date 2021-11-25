0d0abaf657413a3aa1d4997197b9d8baf8fb105b
Bartol Deak <b@bdeak.net>
2021-11-25 11:04:35 +0100

    add Call.Print

--- phonebook/Program.cs
+++ phonebook/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using phonebook.entity;
+using System.Linq;
 
 namespace phonebook
 {
@@ -24,13 +25,15 @@ namespace phonebook
             var contacts = new Dictionary<Contact, Call[]>
             {
                 { new Contact("Ante Antic", "0981234567", ContactType.Favorite),
-                  new Call[] { new Call(new DateTime(2021, 2, 2), Call.Status.Ended), new Call() } },
+                  new Call[] { new Call("0951234567", new DateTime(2021, 2, 2), CallStatus.Ended),
+                               new Call("0961234567") } },
 
                 { new Contact("Ana Anic", "0961234567", ContactType.Favorite),
-                  new Call[] { new Call(new DateTime(2021, 3, 2), Call.Status.Ended) } },
+                  new Call[] { new Call("0981234567", new DateTime(2021, 3, 1), CallStatus.Missed),
+                               new Call("0981234567", new DateTime(2021, 3, 2), CallStatus.Ended) } },
 
                 { new Contact("Marija Maric", "0951234567"),
-                  new Call[] { new Call(new DateTime(2021, 11, 11), Call.Status.Ended) } },
+                  new Call[] { new Call("0981234568", new DateTime(2021, 11, 11), CallStatus.Ended) } },
 
                 { new Contact("Mate Matic", "0991234567", ContactType.Blocked),
                   new Call[] { } },
@@ -96,6 +99,7 @@ namespace phonebook
                         break;
 
                     case Menu.PrintCalls:
+                        Call.Print(new List<Call>(contacts.Values.SelectMany(d => d)));
                         menu = Choice(new string[] { }, menu);
                         break;
                 }
--- phonebook/entity/Call.cs
+++ phonebook/entity/Call.cs
@@ -6,28 +6,59 @@ using System.Threading.Tasks;
 
 namespace phonebook.entity
 {
+    public enum CallStatus
+    {
+        InProgress,
+        Missed,
+        Ended,
+    };
+
     class Call
     {
-        public enum Status
-        {
-            InProgress,
-            Missed,
-            Ended,
-        };
 
+        private string _toNumber;
         private DateTime _date;
-        private Status _status = Status.InProgress;
+        private CallStatus _status = CallStatus.InProgress;
 
-        public Call()
+        public string ToNumber { get => _toNumber; set => _toNumber = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+        public CallStatus Status { get => _status; set => _status = value; }
+
+
+        public Call(string toNumber)
         {
+            _toNumber = toNumber;
             _date = DateTime.Now;
         }
 
-        public Call(DateTime date, Status status)
+        public Call(string toNumber, DateTime date, CallStatus status)
         {
+            _toNumber = toNumber;
             _date = date;
             _status = status;
         }
+
+        static public void Print(List<Call> calls)
+        {
+            Console.WriteLine($"{"Prema broju".PadRight(30)} | {"Datum".PadRight(30)} | Status");
+            Console.WriteLine("------------------------------ | ------------------------------ | ------------------------------");
+
+            foreach (var call in calls)
+            {
+                if (call.Status is CallStatus.Missed)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                if (call.Status is CallStatus.InProgress)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+                Console.WriteLine($"{call.ToNumber.PadRight(30)} | {call.Date.ToString().PadRight(30)} | {call.Status}");
+                Console.ResetColor();
+            }
+        }
+
 
     }
 }
