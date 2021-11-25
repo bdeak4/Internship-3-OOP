01f523478b6f0515e92fa45bd7a2f64c642a26ea
Bartol Deak <b@bdeak.net>
2021-11-25 01:21:41 +0100

    add Contact.Print

--- phonebook/Program.cs
+++ phonebook/Program.cs
@@ -8,13 +8,13 @@ namespace phonebook
     {
         static void Main(string[] args)
         {
-            var data = new Dictionary<Contact, Call[]>
+            var contacts = new Dictionary<Contact, Call[]>
             {
-                { new Contact("Ante Antic", "0981234567", Contact.Type.Favorite),
+                { new Contact("Ante Antic", "0981234567", ContactType.Favorite),
                   new Call[] { new Call(new DateTime(2021, 2, 2), Call.Status.Ended), new Call() } },
                 { new Contact("Ana Anic", "0951234567"),
                   new Call[] { new Call(new DateTime(2021, 11, 11), Call.Status.Ended) } },
-                { new Contact("Mate Matic", "0991234567", Contact.Type.Blocked),
+                { new Contact("Mate Matic", "0991234567", ContactType.Blocked),
                   new Call[] { } },
             };
             int menu = 0;
@@ -41,6 +41,7 @@ namespace phonebook
                         }
                         break;
                     case 1:
+                        Contact.Print(new List<Contact>(contacts.Keys));
                         menu = Choice(new string[] { }, menu);
                         break;
                     case 2:
--- phonebook/entity/Contact.cs
+++ phonebook/entity/Contact.cs
@@ -6,18 +6,22 @@ using System.Threading.Tasks;
 
 namespace phonebook.entity
 {
+    public enum ContactType
+    {
+        Normal,
+        Favorite,
+        Blocked,
+    };
+
     class Contact
     {
-        public enum Type
-        {
-            Normal,
-            Favorite,
-            Blocked,
-        };
-
         private string _name;
         private string _number;
-        private Type _type = Type.Normal;
+        private ContactType _type = ContactType.Normal;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Number { get => _number; set => _number = value; }
+        public ContactType Type { get => _type; set => _type = value; }
 
         public Contact(string name, string number)
         {
@@ -25,12 +29,33 @@ namespace phonebook.entity
             _number = number;
         }
 
-        public Contact(string name, string number, Type type)
+        public Contact(string name, string number, ContactType type)
         {
             _name = name;
             _number = number;
             _type = type;
         }
 
+        static public void Print(List<Contact> contacts)
+        {
+            Console.WriteLine(
+                "Ime i prezime".PadRight(30) + " | " +
+                "Broj".PadRight(30) + " | Preferenca"
+            );
+            Console.WriteLine("------------------------------ | ------------------------------ | ------------------------------");
+            foreach (var contact in contacts)
+            {
+                if (contact.Type is ContactType.Blocked)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                if (contact.Type is ContactType.Favorite)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(
+                    contact.Name.PadRight(30) + " | " +
+                    contact.Number.PadRight(30) + " | " + 
+                    contact.Type
+                );
+                Console.ResetColor();
+            }
+        }
     }
 }
